7f0db5748fd5f1b7174c6e0ee446b83cf2c9c698
Rossitza Kotelova <30867379+rkotelova@users.noreply.github.com>
2020-08-16 20:12:04 -0400

    Update F16_Solved_ps1c.py

--- pSets/ps1/F16_Solved_ps1c.py
+++ pSets/ps1/F16_Solved_ps1c.py
@@ -79,38 +79,3 @@ if portion_saved > 1.0:
 else:   
     print ("Best savings rate:", portion_saved)  
     print ("Steps in bisection search:", iterations)
-
-
-
-
-
-
-   # def binary_search(x, search_list):
-   #     iterations = 1
-   #     left = 0 # Determines the starting index of the list we have to search in
-   #     right = len(search_list)-1 # Determines the last index of the list we have to search in
-   #     mid = (right + left)//2 # In Python, // means floored division, 
-   #     while search_list[mid] != x: # If this is not our search element
-   #         # If the current middle element is less than x then move the left next to mid
-   #         # Else we move right next to mid
-   #         if  search_list[mid] < x:
-   #             left = mid + 1
-   #         else:
-   #             right = mid - 1
-   #         mid = (right + left)//2
-   #         iterations += 1
-   #     print 'iterations = ',str(iterations)
-   #     return mid
-
-   # print(binary_search(32, [4,8,9,10,24,32,45,56]))
-
-
-
-
-        
-
-    
-
-    
-            
-        
